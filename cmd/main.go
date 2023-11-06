84be18abcfbf14214a58eac3f240fa7433d3ebeb
Francesco Michele Barranca <francescomich99@gmail.com>
2023-11-06 13:52:50 -0500

    added root for testing

--- cmd/main.go
+++ cmd/main.go
@@ -11,14 +11,20 @@ import (
 
 
 func main() {
-	
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet {
+			fmt.Fprintf(w, "Nothing to see here...")
+		} else {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		}
+	})
 	http.HandleFunc("/upload", controller.Upload)
 	http.HandleFunc("/delete/", controller.Delete)
 	http.Handle("/images/", http.StripPrefix("/images/", http.FileServer(http.Dir("uploads"))))
 
 	err := os.Mkdir("uploads", os.ModePerm)
 	if err != nil {
-		log.Fatalf("Could not create uploads directory. Error: %s.\nExiting program...", err.Error())
+		fmt.Printf("Could not create uploads directory. Error: %s.", err.Error())
 	}
 	
 	fmt.Println("Server is running on :8888")
